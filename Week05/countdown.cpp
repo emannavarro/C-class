4537429f22718b576060459b5380ea7d4f102737
voiddiov <32153315+voiddiov@users.noreply.github.com>
2018-11-24 22:31:33 -0800

    Add files via upload

---
+++ Week05/countdown.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Enter your code here
+	cout << "\nNASA Mission Control readying for liftoff\n";
+    for (int countdown = 10; countdown >=1 ; countdown--){
+     cout <<countdown << endl;
+     
+
+
+     }
+    return 0;
+}
