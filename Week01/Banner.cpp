9f437151c7843e172d3669d8417692092351081d
voiddiov <32153315+voiddiov@users.noreply.github.com>
2018-11-24 22:27:27 -0800

    Add files via upload

---
+++ Week01/Banner.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "EEEE  M         M   AAA   N     N  U   U  EEEE  L"<< endl;
+    cout << "E     MM       MM  A   A  N N   N  U   U  E     L"<< endl;
+    cout << "EEEE  M  M   M  M  AAAAA  N  N  N  U   U  EEEE  L"<< endl;
+    cout << "E     M   MM    M  A   A  N   N N  U   U  E     L"<< endl;
+    cout << "EEEE  M    M    M  A   A  N    NN   UUU   EEEE  LLLL"<< endl;
+    return 0;
+}
---
+++ Week01/hello.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World My Name is MAnny!!!!\n";
+    return 0;
+}
